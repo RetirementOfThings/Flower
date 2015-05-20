abfec1d28bad1483ca643f49a1c0b841c27a675a
Piotr Oleksy <piotr.olek@o2.pl>
2015-05-20 21:21:38 +0200

    i think that's all

--- src/Beacon.py
+++ src/Beacon.py
@@ -47,6 +47,21 @@ def on_log(mqttc, obj, level, string):
     print(string)
 
 
+def button_handler(state):
+    if state:
+        print 'alarm'
+        alarm()
+        mqttc.publish("flower/1", 'alarm', 0, True)
+
+
+def button_handler2(state):
+    if state:
+        stop_reminding()
+        mqttc.publish("flower/1", 'clear', 0, True)
+
+
+def knob_handler(pos):
+    mqttc.publish("flower/1", 'movement:y', 0, True)
 
 
 def main():
@@ -57,6 +72,10 @@ def main():
     mqttc.on_subscribe = on_subscribe
     mqttc.connect("127.0.0.1", 1883, 60)
     mqttc.subscribe("flower/1", 0)
+    api.set_handler('button1', button_handler)
+    api.set_handler('button2', button_handler2)
+    api.set_handler('knob', knob_handler)
+    api.command('subscribe', 'knob')
     mqttc.loop_forever()
 
 
